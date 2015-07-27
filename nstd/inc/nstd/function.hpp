839e5bbb9231612f1ac0dea992d287b7f162d040
Avinash Baliga <fifoforlifo@gmail.com>
2015-07-27 03:01:08 -0500

    func_base and func_ref basically working

--- nstd/inc/nstd/function.hpp
+++ nstd/inc/nstd/function.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <functional>
+#include <type_traits>
 
 namespace nstd {
 
@@ -9,11 +10,11 @@ namespace nstd {
     {
     protected:
         typedef TRet(*fn_type)(char* obj, TArgs... args);
-        typedef TRet(*fn_raw)(TArgs... args);
         fn_type m_fn;
         char* m_obj;
 
     public:
+        typedef TRet(*fn_raw)(TArgs... args);
         typedef func_base<TRet, TArgs...> This;
 
         ~func_base()
@@ -24,27 +25,39 @@ namespace nstd {
         func_base(const This& rhs)
             : m_fn(rhs.m_fn), m_obj(rhs.m_obj)
         {}
+        func_base(fn_raw fn)
+            : m_fn(), m_obj()
+        {
+            *this = fn;
+        }
+        template <class FuncObj, typename std::enable_if<!std::is_same<typename std::decay<FuncObj>::type, This>::value && !std::is_same<typename std::decay<FuncObj>::type, fn_raw>::value>::type* = 0>
+        func_base(FuncObj&& fnobj)
+            : m_fn(), m_obj()
+        {
+            *this = fnobj;
+        }
+
         This& operator=(const This& rhs)
         {
             m_fn = rhs.m_fn;
             m_obj = rhs.m_obj;
             return *this;
         }
-        This& operator=(TRet(*fn)(TArgs...))
+        This& operator=(fn_raw fn)
         {
             struct Adapter
             {
                 static TRet invoke(char* obj, TArgs... args)
                 {
                     fn_raw fn = (fn_raw)obj;
-                    return fn(args...);
+                    return fn(std::forward<TArgs>(args)...);
                 }
             };
             m_fn = &Adapter::invoke;
             m_obj = (char*)fn;
             return *this;
         }
-        template <class FuncObj>
+        template <class FuncObj, typename std::enable_if<!std::is_same<typename std::decay<FuncObj>::type, This>::value && !std::is_same<typename std::decay<FuncObj>::type, fn_raw>::value>::type* = 0>
         This& operator=(FuncObj&& fnobj)
         {
             struct Adapter
@@ -52,7 +65,7 @@ namespace nstd {
                 static TRet invoke(char* obj, TArgs... args)
                 {
                     FuncObj&& fnobj = (FuncObj&&)*obj;
-                    return fnobj(args...);
+                    return fnobj(std::forward<TArgs>(args)...);
                 }
             };
             m_fn = &Adapter::invoke;
@@ -71,5 +84,46 @@ namespace nstd {
         }
     };
 
+    template <class TRet, class... TArgs>
+    func_base<TRet, TArgs...> select_func_base(TRet(TArgs...));
+
+    template <class TSig>
+    class func_ref : public decltype(select_func_base(TSig()))
+    {
+    public:
+        typedef decltype(select_func_base(TSig())) Base;
+        typedef func_ref<TSig> This;
+
+        func_ref()
+            : Base()
+        {}
+        func_ref(const This& rhs)
+            : Base(rhs)
+        {}
+        func_ref(typename This::fn_raw fn)
+            : Base(fn)
+        {}
+        template <class FuncObj, typename std::enable_if<!std::is_same<typename std::decay<FuncObj>::type, This>::value && !std::is_same<typename std::decay<FuncObj>::type, typename This::fn_raw>::value>::type* = 0>
+        func_ref(FuncObj&& fnobj)
+            : Base(fnobj)
+        {}
+
+        This& operator=(const This& rhs)
+        {
+            Base::operator=(rhs);
+            return *this;
+        }
+        This& operator=(typename This::fn_raw fn)
+        {
+            Base::operator=(fn);
+            return *this;
+        }
+        template <class FuncObj, typename std::enable_if<!std::is_same<typename std::decay<FuncObj>::type, This>::value && !std::is_same<typename std::decay<FuncObj>::type, typename This::fn_raw>::value>::type* = 0>
+        This& operator=(FuncObj&& fnobj)
+        {
+            Base::operator=(fnobj);
+            return *this;
+        }
+    };
 
 } // namespace nstd
--- tests/function/src/test_function.cpp
+++ tests/function/src/test_function.cpp
@@ -4,11 +4,25 @@
 
 int main()
 {
-    nstd::func_base<double, double> d2d;
+    std::function<double(double)> ddd = (double(*)(double))&fabs;
+    ddd = std::function<double(double)>([](double) {return 1.0;});
+    ddd(1.0);
+
+    nstd::func_base<double, double> d2d = &sin;
+    assert(d2d);
+    d2d = &cos;
+    d2d = nstd::func_base<double, double>();
+    assert(!d2d);
+    d2d = nstd::func_base<double, double>(&fabs);
+    d2d = nstd::func_base<double, double>([](double) {return 1.0;});
+    d2d(1.0);
     d2d = &fabs;
 
     double x = d2d(-1);
     assert(x == 1);
+
+    nstd::func_ref<double(*)(double)> dd;
+    dd = &fabs;
 
     return 0;
 }
