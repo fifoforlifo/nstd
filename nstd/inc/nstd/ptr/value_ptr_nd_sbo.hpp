68f5b370e10b2d1e078027ecbc040b7eaa9e08aa
Avinash Baliga <fifoforlifo@gmail.com>
2016-02-15 10:44:47 -0600

    eliminate implicit down-casts

--- nstd/inc/nstd/ptr/value_ptr_nd_sbo.hpp
+++ nstd/inc/nstd/ptr/value_ptr_nd_sbo.hpp
@@ -83,6 +83,15 @@ namespace nstd {
         char m_sbo_buffer[SboSize];
 
     private:
+        template <class Src, class Dst>
+        struct cast_implicit
+        {
+            typename std::remove_reference<Dst>::type* operator()(typename std::remove_reference<Src>::type* p_src)
+            {
+                return p_src;
+            }
+        };
+
         bool null_init(bool result)
         {
             m_p_interface = nullptr;
@@ -91,6 +100,7 @@ namespace nstd {
             return result;
         }
 
+        // NOTE: leaves the object in an inconsistent state; all callers must subsequently call some *_init*() function
         void release()
         {
             if (m_p_interface)
@@ -99,8 +109,28 @@ namespace nstd {
             }
         }
 
+        // NOTE: copy_init and move_init functions are swritten out to produce clear error messages when incompatible types are assigned
+        // DO NOT collapse these with a template argument like CastToInterface
+        // The difference is:
+        //      error: no matching member function for call to 'copy_init_implicit'
+        //      note: candidate template ignored: substitution failure
+        // versus
+        //      error: assigning to 'Derived *' from incompatible type 'Base *'
+
         template <class Rhs, size_t RhsSboSize>
-        bool copy_init_static(const value_ptr_nd_sbo<Rhs, RhsSboSize>& rhs)
+        bool copy_init_implicit(const value_ptr_nd_sbo<Rhs, RhsSboSize>& rhs)
+        {
+            if (!rhs.m_p_interface)
+            {
+                return null_init(true);
+            }
+            m_p_object = rhs.m_p_cloner->copy(m_sbo_buffer, SboSize, rhs.m_p_object);
+            m_p_interface = ((Rhs*)(m_p_object + ((char*)rhs.m_p_interface - rhs.m_p_object)));
+            m_p_cloner = rhs.m_p_cloner;
+            return true;
+        }
+        template <class Rhs, size_t RhsSboSize>
+        bool copy_init_static_cast(const value_ptr_nd_sbo<Rhs, RhsSboSize>& rhs)
         {
             if (!rhs.m_p_interface)
             {
@@ -126,10 +156,31 @@ namespace nstd {
         }
 
         template <class Rhs, size_t RhsSboSize>
-        bool move_init_static(value_ptr_nd_sbo<Rhs, RhsSboSize>&& rhs)
+        bool move_init_implicit(value_ptr_nd_sbo<Rhs, RhsSboSize>&& rhs)
         {
-            Interface* p_interface = static_cast<Interface*>(rhs.m_p_interface);
-            if (!p_interface)
+            if (!rhs.m_p_interface)
+            {
+                return null_init(true);
+            }
+            if (uintptr_t((char*)rhs.m_p_interface - rhs.m_sbo_buffer) < RhsSboSize)
+            {
+                m_p_object = rhs.m_p_cloner->move(m_sbo_buffer, SboSize, rhs.m_p_object);
+                m_p_interface = ((Rhs*)(m_p_object + ((char*)rhs.m_p_interface - rhs.m_p_object)));
+                rhs.release();
+            }
+            else
+            {
+                m_p_object = rhs.m_p_object;
+                m_p_interface = rhs.m_p_interface;
+            }
+            m_p_cloner = rhs.m_p_cloner;
+            rhs.null_init(true);
+            return true;
+        }
+        template <class Rhs, size_t RhsSboSize>
+        bool move_init_static_cast(value_ptr_nd_sbo<Rhs, RhsSboSize>&& rhs)
+        {
+            if (!rhs.m_p_interface)
             {
                 return null_init(true);
             }
@@ -141,8 +192,8 @@ namespace nstd {
             }
             else
             {
-                m_p_interface = static_cast<Interface*>(rhs.m_p_interface);
                 m_p_object = rhs.m_p_object;
+                m_p_interface = static_cast<Interface*>(rhs.m_p_interface);
             }
             m_p_cloner = rhs.m_p_cloner;
             rhs.null_init(true);
@@ -164,22 +215,13 @@ namespace nstd {
             }
             else
             {
-                m_p_interface = p_interface;
                 m_p_object = rhs.m_p_object;
+                m_p_interface = p_interface;
             }
             m_p_cloner = rhs.m_p_cloner;
             rhs.null_init(true);
             return true;
         }
-
-        template <class Src, class Dst>
-        struct cast_implicitly
-        {
-            typename std::remove_reference<Dst>::type* operator()(typename std::remove_reference<Src>::type* p_src)
-            {
-                return p_src;
-            }
-        };
 
         template <class Object, class CastToInterface>
         bool assign_object_value(Object&& obj, CastToInterface&& cast_to_interface)
@@ -220,117 +262,117 @@ namespace nstd {
         }
         value_ptr_nd_sbo(const This& rhs)
         {
-            copy_init_static(rhs);
+            copy_init_implicit(rhs);
         }
         value_ptr_nd_sbo(This& rhs)
         {
-            copy_init_static(rhs);
+            copy_init_implicit(rhs);
         }
         value_ptr_nd_sbo(const This&& rhs)
         {
-            copy_init_static(rhs);
+            copy_init_implicit(rhs);
         }
         value_ptr_nd_sbo(This&& rhs)
         {
-            move_init_static(std::move(rhs));
+            move_init_implicit(std::move(rhs));
         }
         template <class Rhs, size_t RhsSboSize>
         value_ptr_nd_sbo(const value_ptr_nd_sbo<Rhs, RhsSboSize>& rhs)
         {
-            copy_init_static(rhs);
+            copy_init_implicit(rhs);
         }
         template <class Rhs, size_t RhsSboSize>
         value_ptr_nd_sbo(value_ptr_nd_sbo<Rhs, RhsSboSize>& rhs)
         {
-            copy_init_static(rhs);
+            copy_init_implicit(rhs);
         }
         template <class Rhs, size_t RhsSboSize>
         value_ptr_nd_sbo(const value_ptr_nd_sbo<Rhs, RhsSboSize>&& rhs)
         {
-            copy_init_static(rhs);
+            copy_init_implicit(rhs);
         }
         template <class Rhs, size_t RhsSboSize>
         value_ptr_nd_sbo(value_ptr_nd_sbo<Rhs, RhsSboSize>&& rhs)
         {
-            move_init_static(std::move(rhs));
+            move_init_implicit(std::move(rhs));
         }
         template <class Object>
         value_ptr_nd_sbo(Object&& obj)
             : m_p_interface(), m_p_object(), m_p_cloner()
         {
-            assign_object_value(std::forward<Object>(obj), cast_implicitly<Object, Interface>());
+            assign_object_value(std::forward<Object>(obj), cast_implicit<Object, Interface>());
         }
 
         This& operator=(const This& rhs)
         {
             release();
-            copy_init_static(rhs);
+            copy_init_implicit(rhs);
             return *this;
         }
         This& operator=(This& rhs)
         {
             release();
-            copy_init_static(rhs);
+            copy_init_implicit(rhs);
             return *this;
         }
         This& operator=(const This&& rhs)
         {
             release();
-            copy_init_static(rhs);
+            copy_init_implicit(rhs);
             return *this;
         }
         This& operator=(This&& rhs)
         {
             release();
-            move_init_static(std::forward<This>(rhs));
+            move_init_implicit(std::move<This>(rhs));
             return *this;
         }
         template <class Rhs, size_t RhsSboSize>
         This& operator=(const value_ptr_nd_sbo<Rhs, RhsSboSize>& rhs)
         {
             release();
-            copy_init_static(rhs);
+            copy_init_implicit(rhs);
             return *this;
         }
         template <class Rhs, size_t RhsSboSize>
         This& operator=(value_ptr_nd_sbo<Rhs, RhsSboSize>& rhs)
         {
             release();
-            copy_init_static(rhs);
+            copy_init_implicit(rhs);
             return *this;
         }
         template <class Rhs, size_t RhsSboSize>
         This& operator=(const value_ptr_nd_sbo<Rhs, RhsSboSize>&& rhs)
         {
             release();
-            copy_init_static(rhs);
+            copy_init_implicit(rhs);
             return *this;
         }
         template <class Rhs, size_t RhsSboSize>
         This& operator=(value_ptr_nd_sbo<Rhs, RhsSboSize>&& rhs)
         {
             release();
-            move_init_static(std::move(rhs));
+            move_init_implicit(std::move(rhs));
             return *this;
         }
         template <class Object>
         This& operator=(Object&& obj)
         {
             release();
-            assign_object_value(std::forward<Object>(obj), cast_implicitly<Object, Interface>());
+            assign_object_value(std::forward<Object>(obj), cast_implicit<Object, Interface>());
             return *this;
         }
 
-        template <class Object, class CastToInterface = cast_implicitly<Object, Interface>>
-        This& assign(Object&& obj, CastToInterface&& cast_to_interface= cast_implicitly<Object, Interface>())
+        template <class Object, class CastToInterface = cast_implicit<Object, Interface>>
+        This& assign(Object&& obj, CastToInterface&& cast_to_interface= cast_implicit<Object, Interface>())
         {
             release();
             assign_object_value(std::forward<Object>(obj), std::forward<CastToInterface>(cast_to_interface));
             return *this;
         }
 
-        template <class Object, class CastToInterface = cast_implicitly<Object, Interface>>
-        This& acquire(Object* p_object, CastToInterface&& cast_to_interface = cast_implicitly<Object, Interface>())
+        template <class Object, class CastToInterface = cast_implicit<Object, Interface>>
+        This& acquire(Object* p_object, CastToInterface&& cast_to_interface = cast_implicit<Object, Interface>())
         {
             release();
             m_p_interface = cast_to_interface(p_object);
@@ -357,12 +399,26 @@ namespace nstd {
         value_ptr_nd_sbo<Other, OtherSboSize> copy_as() const
         {
             value_ptr_nd_sbo<Other, OtherSboSize> other = noinit_t();
+            other.copy_init_static_cast(*this);
+            return other;
+        }
+        template <class Other, size_t OtherSboSize = SboSize>
+        value_ptr_nd_sbo<Other, OtherSboSize> copy_as_dynamic() const
+        {
+            value_ptr_nd_sbo<Other, OtherSboSize> other = noinit_t();
             other.copy_init_dynamic(*this);
             return other;
         }
 
         template <class Other, size_t OtherSboSize = SboSize>
         value_ptr_nd_sbo<Other, OtherSboSize> move_as()
+        {
+            value_ptr_nd_sbo<Other, OtherSboSize> other = noinit_t();
+            other.move_init_static_cast(std::move(*this));
+            return other;
+        }
+        template <class Other, size_t OtherSboSize = SboSize>
+        value_ptr_nd_sbo<Other, OtherSboSize> move_as_dynamic()
         {
             value_ptr_nd_sbo<Other, OtherSboSize> other = noinit_t();
             other.move_init_dynamic(std::move(*this));
--- tests/type_erasure/src/test_value_ptr.cpp
+++ tests/type_erasure/src/test_value_ptr.cpp
@@ -2,12 +2,12 @@
 #include <nstd/ptr/value_ptr_nd.hpp>
 #include <nstd/ptr/value_ptr_nd_sbo.hpp>
 
-struct INum
+struct ISmallNum
 {
-    virtual ~INum() {}
+    virtual ~ISmallNum() {}
     virtual int Value() const = 0;
 };
-struct IBigNum : public INum
+struct IBigNum : public ISmallNum
 {
     virtual int ValueHi() const = 0;
 };
@@ -36,55 +36,17 @@ struct Num : public IBigNum, IFloatNum
     {}
 };
 
-TEST(TypeErasure, value_ptr_nd)
-{
-    typedef nstd::value_ptr_nd<INum> INumValuePtr;
-    typedef nstd::value_ptr_nd<IBigNum> BigNumValuePtr;
-    typedef nstd::value_ptr_nd<Num> NumValuePtr;
-    typedef nstd::value_ptr_nd<IFloatNum> IFloatNumValuePtr;
-
-    {
-        INumValuePtr pNumA = INumValuePtr().acquire(new Num{ 3 });
-        EXPECT_EQ(3, pNumA->Value());
-        INumValuePtr pNumB = pNumA;
-        EXPECT_EQ(3, pNumB->Value());
-        EXPECT_TRUE(&*pNumB != &*pNumA);
-        BigNumValuePtr pNumC = pNumB.copy_as<IBigNum>();
-        EXPECT_EQ(3, pNumC->Value());
-        EXPECT_TRUE(&*pNumC != &*pNumB);
-        IBigNum* pNumC_old = &*pNumC;
-        NumValuePtr pNumD = pNumC.move_as<Num>();
-        EXPECT_TRUE(!pNumC);
-        EXPECT_EQ(pNumC_old, &*pNumD);
-        BigNumValuePtr pNumE = pNumD;
-        EXPECT_TRUE(!!pNumD && !!pNumE);
-        pNumE = std::move(pNumD);
-        EXPECT_TRUE(!pNumD);
-        pNumD = Num(3);
-        IFloatNumValuePtr pNumF = pNumD.copy_as<IFloatNum>();
-        EXPECT_EQ(3.0f, pNumF->ValueFloat());
-        pNumF = pNumD.move_as<IFloatNum>();
-        EXPECT_TRUE(!pNumD);
-        EXPECT_EQ(3.0f, pNumF->ValueFloat());
-        pNumD = pNumF.copy_as<Num>();
-        EXPECT_TRUE(!!pNumD);
-        Num num(8);
-        pNumA = num;
-        EXPECT_EQ(8, pNumA->Value());
-    }
-}
-
 TEST(TypeErasure, value_ptr_nd_sbo)
 {
-    typedef nstd::value_ptr_nd_sbo<INum, 0x80> INumValuePtr;
+    typedef nstd::value_ptr_nd_sbo<ISmallNum, 0x80> ISmallNumValuePtr;
     typedef nstd::value_ptr_nd_sbo<IBigNum, 0x40> BigNumValuePtr;
     typedef nstd::value_ptr_nd_sbo<Num, 0x20> NumValuePtr;
     typedef nstd::value_ptr_nd_sbo<IFloatNum, 0x30> IFloatNumValuePtr;
 
     {
-        INumValuePtr pNumA = INumValuePtr().acquire(new Num{ 3 });
+        ISmallNumValuePtr pNumA = ISmallNumValuePtr().acquire(new Num{ 3 });
         EXPECT_EQ(3, pNumA->Value());
-        INumValuePtr pNumB = pNumA;
+        ISmallNumValuePtr pNumB = pNumA;
         EXPECT_EQ(3, pNumB->Value());
         EXPECT_TRUE(&*pNumB != &*pNumA);
         BigNumValuePtr pNumC = pNumB.copy_as<IBigNum>();
@@ -93,8 +55,10 @@ TEST(TypeErasure, value_ptr_nd_sbo)
         NumValuePtr pNumD = pNumC.move_as<Num>();
         EXPECT_TRUE(!pNumC);
         EXPECT_TRUE(!!pNumD);
+#if 0 // desirable error: assigning to 'Num *' from incompatible type 'INum *'
         pNumD = pNumA;
         EXPECT_TRUE(pNumD->Value() == pNumA->Value());
+#endif
         BigNumValuePtr pNumE = pNumD;
         EXPECT_TRUE(!!pNumD && !!pNumE);
         pNumE = std::move(pNumD);
